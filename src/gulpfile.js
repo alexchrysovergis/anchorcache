eefe120efd3c2b9840a8a3cbcce00b4217b33ddf
alexchrysovergis <alexandroschrysovergis@gmail.com>
2024-01-29 12:40:44 -0800

    Refined gulp tasks

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,10 +1,24 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 function css() {
-    return src('./src/scss/*.scss') // Use a wildcard pattern to match all Sass files
+    return src('./src/scss/*.scss') //the src
         .pipe(sass())
-        .pipe(dest('./dist/css/'));
+        .pipe(concat('styles.min.css'))  // concat the scss into a single file
+        .pipe(cleanCSS())  // minify it
+        .pipe(dest('./dist/css/')); //the compiled css destination
 }
 
 exports.buildCss = css;
+
+function js() {
+    return src('./src/js/*.js') //the src
+        .pipe(concat('scripts.min.js')) // concat the js into one file
+        .pipe(uglify())  // minify it
+        .pipe(dest('./dist/js/')); //the compiled js destination
+}
+
+exports.buildJs = js;
